a56e0f76f05be7f2c1b5150a26ebd7c14ccb638f
fedorkanin <fedorkanin2004@gmail.com>
2021-04-13 22:03:13 +0300

    new DrawVector

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ public:
         this->max_angle = max_angle;
         this->w = 2 * M_PI / T;
         this->angle = max_angle;
-        this->leght = 9.81 * pow(T / (2 * M_1_PI), 2);
+        this->leght = 9.81 * pow(T / ( 2 * M_1_PI ), 2);
         this->mas = mas;
     }
 
@@ -70,10 +70,10 @@ void SetPosition(SHORT x, SHORT y)
 
 
 void DrawPointOld(int x, int y, char sym) {
-    for (int i = 1; i < y; i++) {
+    for ( int i = 1; i < y; i++ ) {
         cout << endl;
     }
-    for (int i = 1; i < x; i++) {
+    for ( int i = 1; i < x; i++ ) {
         cout << ' ';
     }
     cout << sym << endl;
@@ -85,28 +85,28 @@ void DrawPoint(int x, int y, char sym) {
 }
 
 vector<vector<bool>> DrawLineToVector(vector<vector<bool>> mas, int x1, int y1, int x2, int y2, char sym) {
-    double y_x = static_cast<double>(y2 - y1 + 1) / static_cast<double>(x2 - x1 + 1);
-    double x_y = static_cast<double>(x2 - x1 + 1) / static_cast<double>(y2 - y1 + 1);
+    double y_x = static_cast< double >( y2 - y1 + 1 ) / static_cast< double >( x2 - x1 + 1 );
+    double x_y = static_cast< double >( x2 - x1 + 1 ) / static_cast< double >( y2 - y1 + 1 );
 
 
-    for (int y_cur = 1; y_cur <= y2; y_cur++) {
+    for ( int y_cur = 1; y_cur <= y2; y_cur++ ) {
         double x_cur = x_y * y_cur;
-        int x_cur_int = (int)(x_cur);
+        int x_cur_int = ( int )( x_cur );
 
-        if (x_cur_int >= x1 && x_cur_int <= x2) {
-            if (x_cur - floor(x_cur) == 0.5) {
+        if ( x_cur_int >= x1 && x_cur_int <= x2 ) {
+            if ( x_cur - floor(x_cur) == 0.5 ) {
                 mas[y_cur][x_cur_int] = 1;
             }
             mas[y_cur - 1][x_cur_int - 1] = 1;
         }
     }
 
-    for (int x_cur = 1; x_cur <= x2; x_cur++) {
+    for ( int x_cur = 1; x_cur <= x2; x_cur++ ) {
         double y_cur = y_x * x_cur;
-        double y_cur_int = (int)(y_cur);
+        double y_cur_int = ( int )( y_cur );
 
-        if (y_cur_int >= y1 && y_cur_int <= y2) {
-            if (y_cur - floor(y_cur) == 0.5) {
+        if ( y_cur_int >= y1 && y_cur_int <= y2 ) {
+            if ( y_cur - floor(y_cur) == 0.5 ) {
                 mas[y_cur_int][x_cur] = 1;
             }
             mas[y_cur_int - 1][x_cur - 1] = 1;
@@ -118,13 +118,11 @@ vector<vector<bool>> DrawLineToVector(vector<vector<bool>> mas, int x1, int y1, 
     return mas;
 }
 
-void DrawVector(vector<vector<bool>> mas, char sym) {
-    for (auto boka : mas) {
-        for (auto joka : boka) {
-            if (joka) cout << sym;
-            else cout << ' ';
+void DrawVector(vector<vector<bool>> vnuk, char sym) {
+    for ( int boka = 0; boka < vnuk.size(); boka++ ) {
+        for ( int joka = 0; joka < vnuk[boka].size(); joka++ ) {
+            if ( vnuk[boka][joka] ) DrawPoint(joka, boka, sym);
         }
-        cout << endl;
     }
 }
 
@@ -134,20 +132,12 @@ void DrawVector(vector<vector<bool>> mas, char sym) {
 int main() {
     vector<vector<bool>> mas
     {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
+        {1, 1, 0},
+        {0, 1, 0},
+        {0, 1, 0}
     };
 
-    // int k = 2;
-
-    // mas = DrawLineToVector(mas, 1, 1, 1, 2, '1');
-    // DrawVector(mas, '1');
-
-
-    DrawPoint(1, 1, '1');
-    DrawPoint(1, 5, '1');
-    //DrawPoint(1, 1, '\b');
+    DrawVector(mas, '1');
 
     return 0;
 }
