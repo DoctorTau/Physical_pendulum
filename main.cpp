e2dfd5cb608cf522529fb4b0855c86a8999b2186
fedorkanin <fedorkanin2004@gmail.com>
2021-04-17 14:41:28 +0300

    single pendulum works

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 
 using namespace std;
 
-SHORT console_top_offset = 3; //for SetPosition
-int vec_size = 50;
+SHORT console_top_offset = 0; //for SetPosition
+int vec_size = 60;
 vector<vector<bool>> screen_vector(vec_size, vector<bool>(vec_size));
+vector<vector<bool>> screen_vector_old = screen_vector;
 
 void SetPosition(SHORT x, SHORT y)
 {
@@ -31,12 +32,18 @@ void DrawPoint(int x, int y, char sym) {
 void DrawVector(vector<vector<bool>>& vnuk, char sym) {
     for ( int boka = 0; boka < vnuk.size(); boka++ ) {
         for ( int joka = 0; joka < vnuk[boka].size(); joka++ ) {
-            if ( vnuk[boka][joka] ) DrawPoint(joka, boka, sym);
+            if ( vnuk[boka][joka] ) {
+                DrawPoint(joka, boka, sym);
+            }
+            else {
+                if ( screen_vector_old[boka][joka] ) {
+                    DrawPoint(joka, boka, ' ');
+                }
+            }
         }
     }
 }
 
-//draws screen_vector
 void DrawVectorIron() {
     for ( auto boka : screen_vector ) {
         for ( auto joka : boka ) {
@@ -51,74 +58,6 @@ void ClearConsole() {
     cout << "\x1B[2J\x1B[H";
 }
 
-void DrawPointOld(int x, int y, char sym) {
-    for ( int i = 1; i < y; i++ ) {
-        cout << endl;
-    }
-    for ( int i = 1; i < x; i++ ) {
-        cout << ' ';
-    }
-    cout << sym << endl;
-}
-
-vector<vector<bool>> DrawLineToVectorOld(vector<vector<bool>>& mas, int x1, int y1, int x2, int y2) {
-    int x_offset = 10;
-    x1 += x_offset;
-    x2 += x_offset;
-    double y_x = static_cast< double >( y2 - y1 + 1 ) / static_cast< double >( x2 - x1 + 1 );
-    double x_y = static_cast< double >( x2 - x1 + 1 ) / static_cast< double >( y2 - y1 + 1 );
-
-    for ( int y_cur = y1; y_cur <= y2; y_cur++ ) {
-        double x_cur = x_y * y_cur;
-        int x_cur_int = ( int )( x_cur );
-
-        if ( x_cur_int >= x1 && x_cur_int <= x2 ) {
-            if ( x_cur - floor(x_cur) == 0.5 ) {
-                mas[y_cur][x_cur_int] = 1;
-            }
-            mas[y_cur - 1][x_cur_int - 1] = 1;
-        }
-    }
-
-    for ( int x_cur = x1; x_cur <= x2; x_cur++ ) {
-        double y_cur = y_x * x_cur;
-        double y_cur_int = ( int )( y_cur );
-
-        if ( y_cur_int >= y1 && y_cur_int <= y2 ) {
-            if ( y_cur - floor(y_cur) == 0.5 ) {
-                mas[y_cur_int][x_cur] = 1;
-            }
-            mas[y_cur_int - 1][x_cur - 1] = 1;
-        }
-    }
-    return mas;
-}
-
-vector<vector<bool>> DrawLineToVector(vector<vector<bool>>& mas, int x1, int y1, int x2, int y2) {
-    int deltax = abs(x2 - x1);
-    int deltay = abs(y2 - y1);
-    int error = 0;
-    int deltaerr = ( deltay + 1 );
-    int y = y1;
-    int diry = y2 - y1;
-
-    if ( diry > 0 ) {
-        diry = 1;
-    }
-    if ( diry < 0 ) {
-        diry = -1;
-    }
-    for ( int x = x1; x < x2; x++ ) {
-        mas[y - 1][x - 1] = 1;
-        error += deltaerr;
-        if ( error >= deltax + 1 ) {
-            y += diry;
-            error -= ( deltax + 1 );
-        }
-    }
-    return mas;
-}
-
 struct Point {
     int x;
     int y;
@@ -127,6 +66,51 @@ struct Point {
         this->y = y;
     }
 };
+
+void DrawLineToVector(int x1, int y1, int x2, int y2) {
+    Point p1; Point p2; Point p_tmp;
+    p1.x = x1; p1.y = y1;
+    p2.x = x2; p2.y = y2;
+    if ( p1.x > p2.x ) {
+        p_tmp = p2;
+        p2 = p1;
+        p1 = p_tmp;
+    }
+    x1 = p1.x; x2 = p2.x;
+    y1 = p1.y; y2 = p2.y;
+
+
+    const int deltaX = abs(x2 - x1);
+    const int deltaY = abs(y2 - y1);
+    const int signX = x1 < x2 ? 1 : -1;
+    const int signY = y1 < y2 ? 1 : -1;
+    int error = deltaX - deltaY;
+    screen_vector[y2][x2] = 1;
+    while ( x1 != x2 || y1 != y2 )
+    {
+        screen_vector[y1][x1] = 1;
+        int error2 = error * 2;
+        if ( error2 > -deltaY )
+        {
+            error -= deltaY;
+            x1 += signX;
+        }
+        if ( error2 < deltaX )
+        {
+            error += deltaX;
+            y1 += signY;
+        }
+    }
+
+}
+
+void ClearVector() {
+    for ( int boka = 0; boka < screen_vector.size(); boka++ ) {
+        for ( int joka = 0; joka < screen_vector[boka].size(); joka++ ) {
+            screen_vector[boka][joka] = 0;
+        }
+    }
+}
 
 class Pendulum {
     Point top;
@@ -137,6 +121,8 @@ class Pendulum {
     double w;
     double max_angle;
     double length;
+    int offset_x;
+    int offset_y;
 
 public:
 
@@ -147,41 +133,40 @@ public:
         this->max_angle = max_angle;
         this->w = 2 * M_PI / T;
         this->angle = max_angle;
-        this->length = 9.81 * pow(T / ( 2 * M_PI ), 2);
         this->mas = mas;
 
-        this->body.x = floor(r * sin(this->angle));
-        this->body.y = floor(r * cos(this->angle));
+        this->body.x = x_top + floor(r * sin(this->angle));
+        this->body.y = y_top + floor(r * cos(this->angle));
+
+        this->offset_x = x_top;
+        this->offset_y = y_top;
     }
 
     void Draw() {
-        screen_vector = DrawLineToVector(screen_vector,
-            min(this->top.x, this->body.x),
-            min(this->top.y, this->body.y),
-            max(this->top.x, this->body.x),
-            max(this->top.y, this->body.y));
-        DrawVectorIron();
-        //DrawVector(screen_vector, '1');
-        //DrawPoint(this->body.x, this->body.y, '@');
+        screen_vector_old = screen_vector;
+        ClearVector();
+        DrawLineToVector(this->top.x, this->top.y, this->body.x, this->body.y);
+        DrawVector(screen_vector, '1');
+        DrawPoint(this->body.x, this->body.y, '@');
     }
 
     void Process(double t) {
         this->angle = this->max_angle * cos(this->w * t + this->max_angle);
-        this->body.x = floor(r * sin(this->angle));
-        this->body.y = floor(r * cos(this->angle));
+        this->body.x = this->top.x + floor(r * sin(this->angle));
+        this->body.y = this->top.y + floor(r * cos(this->angle));
     }
 
 
 };
 
-int x = 30;
+int x = 25;
 int y = 1;
-int r = 10;
+int r = 15;
 double T = 4;
-double start_angle = M_PI / 4;
+double start_angle = 5 * M_PI / 11;
 double mas = 1;
 double g = 9.8;
-int fps = 2;
+int fps = 60;
 
 int main() {
     clock_t before = clock();
@@ -194,14 +179,8 @@ int main() {
         double internal_time = ( float )( internal_time_ms ) / 1000;
         pend.Process(internal_time);
         pend.Draw();
-        //cout << internal_time << endl;
         Sleep(1000 / fps);
     }
-
-    //screen_vector = DrawLineToVector(screen_vector, 1, 1, 19, 10);
-    //DrawVector(screen_vector, '1');
-
-
 
     return 0;
 }
